464809c57fe9c722de9579ece506e2392979f045
Roger-luo <hiroger@qq.com>
2019-04-05 03:27:41 +0800

    Files generated by PkgTemplates

---
+++ src/YaoBenchmarks.jl
@@ -0,0 +1,5 @@
+module YaoBenchmarks
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using YaoBenchmarks
+using Test
+
+@testset "YaoBenchmarks.jl" begin
+    # Write your own tests here.
+end
